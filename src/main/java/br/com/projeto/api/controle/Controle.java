f65e15b55a1c634fb4af38f8b37c0765bb6faaca
rjunio98 <robledorjunior1@gmail.com>
2023-12-03 02:07:35 -0300

    utilizando pathvariable

--- src/main/java/br/com/projeto/api/controle/Controle.java
+++ src/main/java/br/com/projeto/api/controle/Controle.java
@@ -1,6 +1,7 @@
 package br.com.projeto.api.controle;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,15 @@ public class Controle {
     @GetMapping("/")
     public String mensagem(){
         return "Olá, mundo!";
+    }
+
+    @GetMapping("/boasVindas")
+    public String boasVindas(){
+        return "Bem vindo" ;
+    }
+
+    @GetMapping("/boasVindas/{nome}")
+    public String boasVindas(@PathVariable String nome){
+        return "Bem vindo " + nome;
     }
 }
