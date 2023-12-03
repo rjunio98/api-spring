c5f9dc3d3b1b424714e29030f957d352844b263a
rjunio98 <robledorjunior1@gmail.com>
2023-12-03 01:56:41 -0300

    implementando controle

---
+++ src/main/java/br/com/projeto/api/controle/Controle.java
@@ -0,0 +1,13 @@
+package br.com.projeto.api.controle;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controle {
+    
+    @GetMapping("/")
+    public String mensagem(){
+        return "Olá, mundo!";
+    }
+}
