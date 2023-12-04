5554eef67b93b687b3b45baaa86f62e523efe930
rjunio98 <robledorjunior1@gmail.com>
2023-12-04 01:39:11 -0300

    efetuando cadastros com save

--- src/main/java/br/com/projeto/api/controle/Controle.java
+++ src/main/java/br/com/projeto/api/controle/Controle.java
@@ -15,6 +15,11 @@ public class Controle {
 
     @Autowired
     private Repositorio acao;
+
+    @PostMapping("/api")
+    public Pessoa cadastrar(@RequestBody Pessoa obj){
+        return acao.save(obj);
+    }
     
     @GetMapping("/")
     public String mensagem(){
